3a9a4aa1c43e492e0d51a6495e1c88c4d6a5d9e2
Alejo Silva <alejo.lautaro@hotmail.com>
2026-09-02 18:34:35 -0300

    feat: New background image and blur effect on lockscreen

--- i3/.config/i3/lockscreen.sh
+++ i3/.config/i3/lockscreen.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # ~/.config/i3/lockscreen.sh
 
-betterlockscreen -u ~/Pictures/Backgrounds/rebranding-alnixdev-v4.png
+betterlockscreen -u ~/Pictures/Backgrounds/IMG_20250709_082307_edited.jpg
 
 back_to_black=30
 (sleep 1 && xset dpms force off && xset dpms $back_to_black $back_to_black $back_to_black) &
 
-betterlockscreen -l
+betterlockscreen -l blur
 
 timeout=600
 xset s $timeout $timeout

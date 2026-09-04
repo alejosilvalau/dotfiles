a3db14e1553f1e5cd46fbae5230034778d5100a8
Alejo Silva <alejo.lautaro@hotmail.com>
2026-09-04 16:54:48 -0300

    fix: Lockscreen not detecting when timeout is disabled

--- i3/.config/i3/lockscreen.sh
+++ i3/.config/i3/lockscreen.sh
@@ -5,6 +5,7 @@ notify-send -u normal -t 6000 "󰌾 Locking Screen" "Screen will lock shortly...
 
 betterlockscreen -u ~/Pictures/Backgrounds/IMG_20250709_082307_edited.jpg
 
+saved_dpms_enabled=$(xset q 2>/dev/null | grep "DPMS is" | awk '{print $3}')
 saved_timeout=$(xset q 2>/dev/null | grep "Standby:" | awk '{print $2}')
 saved_timeout=${saved_timeout:-600}
 
@@ -13,8 +14,12 @@ back_to_black=30
 
 betterlockscreen -l blur
 
-timeout=$saved_timeout
-xset s $timeout $timeout
-xset dpms $timeout $timeout $timeout
-
-notify-send -u normal -t 4000 "󰌾 Screen Unlocked" "Screen timeout restored to $((timeout / 60)) min"
+if [ "$saved_dpms_enabled" = "Enabled" ]; then
+  xset s $saved_timeout $saved_timeout
+  xset dpms $saved_timeout $saved_timeout $saved_timeout
+  # notify-send -u normal -t 4000 "󰌾 Screen Unlocked" "Screen timeout restored to $((saved_timeout / 60)) min"
+else
+  xset s off
+  xset -dpms
+  # notify-send -u normal -t 4000 "󰌾 Screen Unlocked" "Screen timeout has been disabled"
+fi
